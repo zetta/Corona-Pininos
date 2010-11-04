637070796cef9c749bc337d13ee51dd7f8729b5a
zetta <zetaweb@gmail.com>
2010-11-03 19:07:35 -0600

    cambios

--- Boomshine/main.lua
+++ Boomshine/main.lua
@@ -4,6 +4,7 @@
 display.setStatusBar( display.HiddenStatusBar )
 
 
+
 --[[
 -- comenzamos con la fisica 
 local physics = require("physics")
@@ -32,6 +33,7 @@ BALLS = 15
 balls = {}
 
 
+local bg = display.newImage("Aurora.png")
 
 -- iniciamos las pelotitas 
 
@@ -39,46 +41,72 @@ balls = {}
 
 
 for i = 0,BALLS do
-    circle = display.newCircle( math.random(WIDTH), math.random(HEIGHT), 7 )
-    circle:setFillColor(math.random(255),math.random(255),math.random(255))
+    circle = display.newCircle( math.random(WIDTH-5), math.random(HEIGHT-5), 9 )
+    circle:setFillColor(math.random(255),math.random(255),math.random(255), 180)
+    circle:setStrokeColor(100,100,100)
     circle.speed = math.random(2,6)
     circle.direction = math.random(0,1) < 1 and -1 or 1
     circle.speed = circle.speed*circle.direction
     circle.speedOnX, circle.speedOnY = circle.speed, circle.speed -- multiple asignation ¬¬ esta re chafa, multiple asignation deberia ser cortito!!
-
-
     -- physics.addBody( circle, { density = 1.0, friction = 0.3, bounce = 0.2, radius = 7 } )
     balls[i] = circle
 end 
 
 
-Runtime:addEventListener( "enterFrame", function()
-    for index,ball in pairs(balls) do 
-        ball.x = ball.x + ball.speedOnX
-        ball.y = ball.y + ball.speedOnY
 
-        if ball.x >= WIDTH or ball.x <= 0 then
-            ball.speedOnX = ball.speedOnX*-1
+Runtime:addEventListener( "enterFrame",
+    function()
+        for index,ball in pairs(balls) do 
+
+            -- vamos a checar la colision
+            for i,otherBall in pairs(balls) do
+                if  otherBall.speed == 0 then
+                    -- print('otherBall')
+                    i = otherBall.x-ball.x
+                    j = otherBall.y-ball.y
+                    magnitude = math.sqrt( (i*i)+(j*j) )
+                    -- print('----------------')                    
+                    -- print(magnitude)
+                    -- print(otherBall.xScale*7)
+                    if ( magnitude < 40) then
+                        ball.speed, ball.speedOnX, ball.speedOnY = 0,0,0
+                        -- print('colission')
+                    end
+                end
+            end
+
+            ball.x = ball.x + ball.speedOnX
+            ball.y = ball.y + ball.speedOnY
+
+            if ball.speed == 0 then
+                if ball.xScale < 6 then
+                    ball:scale(1.1,1.1)
+                end
+            else
+                if ball.x >= WIDTH or ball.x <= 0 then
+                    ball.speedOnX = ball.speedOnX*-1
+                end
+                if ball.y >= HEIGHT or ball.y <= 0 then
+                    ball.speedOnY = ball.speedOnY*-1
+                end
+            end
         end
-
-        if ball.y >= HEIGHT or ball.y <= 0 then
-            ball.speedOnY = ball.speedOnY*-1
-        end 
     end
-end )
+)
 
 
 
 
-
-
-
-
-
-
-
-
-
+Runtime:addEventListener("touch",
+    function(event)
+        circle = display.newCircle( event.x, event.y, 9 )
+        circle:setFillColor(255,221,0,180)
+        circle:setStrokeColor(100,100,100)
+        circle.direction = 1
+        circle.speed, circle.speedOnX, circle.speedOnY = 0,0,0
+        table.insert(balls, circle)
+    end
+)
 
 
 
